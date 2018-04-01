2bd6bfe20b99bb80597a9c760a957e684d4d7ea5
kahfi <kahfi@github.com>
2018-04-01 22:07:24 +0700

    saya sudah lemas mass

---
+++ app/src/main/java/id/ac/telkomuniversity/student/idofirdaus/kahfiahadian_1202154215_modul6/DBcomment.java
@@ -0,0 +1,31 @@
+package id.ac.telkomuniversity.student.idofirdaus.kahfiahadian_1202154215_modul6;
+
+public class DBcomment {
+    String userKomentar, isiKomentar, fotoKomentar;
+
+    //Dibutuhkan kosong untuk membaca data
+    public DBcomment(){
+
+    }
+
+    //Constructor dari class ini
+    public DBcomment(String userKomentar, String isiKomentar, String fotoKomentar) {
+        this.userKomentar = userKomentar;
+        this.isiKomentar = isiKomentar;
+        this.fotoKomentar = fotoKomentar;
+    }
+
+    //getter untuk variabel dari class ini
+    public String getFotoKomentar() {
+        return fotoKomentar;
+    }
+
+    public String getUserKomentar() {
+        return userKomentar;
+    }
+
+    public String getIsiKomentar() {
+        return isiKomentar;
+    }
+
+}
